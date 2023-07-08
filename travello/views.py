a35f73b3fb8d1b32f69074d7f591457eef1ae185
Venkata Naveen Reddy Yaparla <yaparla999@gmail.com>
2023-07-08 20:22:52 +0530

    origin

--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from .models import Destination 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    dest1=Destination()
+    dest1.name='mumbai'
+    dest1.desc='the city that never sleep '
+    dest1.img ='destination_1.jpg'
+    dest1.price =700
+
+    dest2=Destination()
+    dest2.name='hyderabad'
+    dest2.desc='the city that never sleep '
+    dest2.img ='destination_2.jpg'
+    dest2.price =7020
+
+    dests=[dest1,dest2]
+    return render(request,"index.html",{'dests':dests})
